545067c899c7d8aaa6d0e227b2326b71774e0008
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2024-04-06 15:48:59 -0300

    feat: Repositorio Entidade
    Adição das Classes "DemoRepositorio" e "Repositorio Arquivo", que servem para a manipulação dos objetos dos arquivos.

--- donpipe/src/main/java/com/devxminds/donpipe/entidades/RepositorioArquivo.java
+++ donpipe/src/main/java/com/devxminds/donpipe/entidades/RepositorioArquivo.java
@@ -1,24 +1,36 @@
 package com.devxminds.donpipe.entidades;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ * A classe RepositorioArquivo implementa a interface DemoRepositorio para manipulação de objetos Arquivo.
+ * Esta implementação utiliza um mapa para armazenar os arquivos em memória.
+ */
 import org.springframework.stereotype.Repository;
-
 import java.util.HashMap;
 import java.util.Map;
 
 @Repository
 public class RepositorioArquivo implements DemoRepositorio<Arquivo> {
     private Map<Long, Arquivo> repositorio;
-
     public RepositorioArquivo(){
         this.repositorio = new HashMap<>();
     }
 
+    /**
+     * Salva o arquivo fornecido no repositório.
+     *
+     * @param arquivo o arquivo a ser salvo.
+     */
     @Override
     public void save(Arquivo arquivo) {
         repositorio.put(arquivo.getId(), arquivo);
     }
 
+    /**
+     * Retorna o arquivo pelo ID fornecido.
+     *
+     * @param id o ID do arquivo a ser retornado.
+     * @return o arquivo correspondente ao ID fornecido, ou null se não encontrado.
+     */
     @Override
     public Arquivo findArquivoById(Long id) {
         return repositorio.get(id);
