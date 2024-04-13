9db3c422b95e892d8e2b1c9d39281c6c3f5c3819
Caio Sousa <caui.sousa5@fatec.sp.gov.br>
2024-04-13 20:04:22 -0300

    Atualização de estilos CSS, remoção de link de menu não utilizado e adição de função de conversão de CSV no script.js

--- donpipe/src/main/resources/static/js/script.js
+++ donpipe/src/main/resources/static/js/script.js
@@ -94,4 +94,21 @@ document.addEventListener('DOMContentLoaded', function () {
     logoutBtn.addEventListener('click', () => {
         alert('Você fez logout!');
     });
+
+    function converterCSV() {
+        var fileInput = document.getElementById('csvFile');
+        var file = fileInput.files[0];
+        var formData = new FormData();
+        formData.append('arquivo', file);
+    
+        fetch('/load', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('jsonResult').innerText = JSON.stringify(data, null, 2);
+        })
+        .catch(error => console.error('Erro:', error));
+    } 
 });
