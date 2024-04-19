66c830d9a967ad915b9bd54b20974d30dfa24d01
AndreWakugawa <andre.wakugawa@outlook.com>
2024-04-19 13:26:15 -0300

    fix: ajuste de entities e DTOs ->novas migrations

--- donpipe/src/main/java/com/devxminds/donpipe/dto/UserDto.java
+++ donpipe/src/main/java/com/devxminds/donpipe/dto/UserDto.java
@@ -1,22 +1,22 @@
 package com.devxminds.donpipe.dto;
 
 import com.devxminds.donpipe.entidade.Empresa;
+import com.devxminds.donpipe.entidade.Permissao;
 import lombok.Value;
-
-import java.io.Serializable;
 
 /**
  *Data Transfer Object (DTO) para objetos User. Serializa Jsons em Objetos UserDTO.
  *<p>
  *DTO for {@link com.devxminds.donpipe.entidade.User}
  *@author Caue
- *@version 0.1
+ *@version 1.1
  */
 @Value
-public class UserDto implements Serializable {
+public class UserDto{
     Integer id;
     String nomeUser;
     String email;
     String senha;
     Empresa idEmpresa;
+    Permissao permissao;
 }
