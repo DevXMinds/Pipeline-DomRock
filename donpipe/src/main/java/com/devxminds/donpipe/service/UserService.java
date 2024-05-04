ec01832bf58f74e24da6a3c20ffd3b7215c68fde
CauevSilv <cauevsilva2002@gmail.com>
2024-05-04 12:59:10 -0300

    update: Services
    Movimentação de arquivos

--- donpipe/src/main/java/com/devxminds/donpipe/service/UserService.java
+++ donpipe/src/main/java/com/devxminds/donpipe/service/UserService.java
@@ -2,13 +2,12 @@ package com.devxminds.donpipe.service;
 
 import com.devxminds.donpipe.dto.UserDto;
 import com.devxminds.donpipe.entidade.User;
+import com.devxminds.donpipe.repositorios.PermissaoRepository;
 import com.devxminds.donpipe.repositorios.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -18,9 +17,12 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private PermissaoRepository permissaoRepository;
 
     public UserDto newUser(UserDto userDto) {
         User user = modelMapper.map(userDto, User.class);
+        user.setPermissao(permissaoRepository.findById("lz").get());
         user = userRepository.save(user);
         return userDto;
     }
@@ -32,17 +34,8 @@ public class UserService {
         } else {
             throw new NoSuchElementException("Usuário não encontrado com i id: " + id);
         }
-
     }
 
-    public List<UserDto> findAllUsers() {
-        List<User> users = userRepository.findAll();
-        List<UserDto> userDtos = new ArrayList<>();
-        for (User user : users) {
-            userDtos.add(modelMapper.map(user, UserDto.class));
-        }
-        return userDtos;
-    }
     public UserDto getUserByEmail(String email) {
         Optional<User> user = userRepository.findByEmail(email);
         if (user.isPresent()) {
